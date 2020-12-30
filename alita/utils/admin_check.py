236cc25af50785b5c4c9e4b2ee9eda71c41a8583
Skuzzy xD <SkuzzyxD@users.noreply.github.com>
2020-12-30 13:46:31 +0530

    Devs and owners can perform admin and owner commands!

--- alita/utils/admin_check.py
+++ alita/utils/admin_check.py
@@ -1,10 +1,13 @@
 from alita.__main__ import Alita
-from pyrogram.types import Message
+from pyrogram.types import Message, OWNER_ID, DEV_USERS
 
 
 async def admin_check(c: Alita, m: Message) -> bool:
     chat_id = m.chat.id
     user_id = m.from_user.id
+    
+    if int(user_id) == int(OWNER_ID) or int(user_id) in DEV_USERS:
+        return True
 
     user = await c.get_chat_member(chat_id=chat_id, user_id=user_id)
     admin_strings = ["creator", "administrator"]
@@ -21,6 +24,9 @@ async def admin_check(c: Alita, m: Message) -> bool:
 async def owner_check(c: Alita, m: Message) -> bool:
     chat_id = m.chat.id
     user_id = m.from_user.id
+    
+    if int(user_id) == int(OWNER_ID) or int(user_id) in DEV_USERS:
+        return True
 
     user = await c.get_chat_member(chat_id=chat_id, user_id=user_id)
 
