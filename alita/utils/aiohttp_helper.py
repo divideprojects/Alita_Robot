fc44ac67a0016dfbcfc5e178ef6ec65d0279f04a
Divkix <techdroidroot@gmail.com>
2021-02-11 09:24:23 +0530

    Create aiohttp_helper.py

---
+++ alita/utils/aiohttp_helper.py
@@ -0,0 +1,23 @@
+from aiohttp import ClientSession
+
+
+class AioHttp:
+    """class for helping get the data from url using aiohttp."""
+
+    @staticmethod
+    async def get_json(link):
+        async with ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json(), resp
+
+    @staticmethod
+    async def get_text(link):
+        async with ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.text(), resp
+
+    @staticmethod
+    async def get_raw(link):
+        async with ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.read(), resp
