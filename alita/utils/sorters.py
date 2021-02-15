ef23af4029df5a6331bd697efb615f50fe8da48a
Divkix <techdroidroot@gmail.com>
2021-02-15 13:19:34 +0530

    fix adminlist sorter

--- alita/utils/sorters.py
+++ alita/utils/sorters.py
@@ -1,10 +1,10 @@
 # Sorts adminlist by second tuple place in item in it
 async def sort_adminlist(adminlist):
-    lst_len = len(lst)
+    lst_len = len(adminlist)
     for i in range(0, lst_len):
         for j in range(0, lst_len - i - 1):
-            if lst[j][1] > lst[j + 1][1]:
-                temp = lst[j]
-                lst[j] = lst[j + 1]
-                lst[j + 1] = temp
-    return lst
+            if adminlist[j][1] > adminlist[j + 1][1]:
+                temp = adminlist[j]
+                adminlist[j] = adminlist[j + 1]
+                adminlist[j + 1] = temp
+    return adminlist
