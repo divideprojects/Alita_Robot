850649e575814eaf4833006a4743656d7804d3fe
Divkix <techdroidroot@gmail.com>
2021-02-18 09:11:30 +0530

    [skip ci] Change Pyrogram Warning Level to Info

--- alita/__init__.py
+++ alita/__init__.py
@@ -18,7 +18,7 @@
 
 from datetime import datetime
 from importlib import import_module as imp_mod
-from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+from logging import INFO, FileHandler, StreamHandler, basicConfig, getLogger
 from os import environ, mkdir, path
 from sys import exit as sysexit
 from sys import stdout, version_info
@@ -42,7 +42,7 @@ basicConfig(
     handlers=[file_handler, stdout_handler],
 )
 
-getLogger("pyrogram").setLevel(WARNING)
+getLogger("pyrogram").setLevel(INFO)
 LOGGER = getLogger(__name__)
 
 # if version < 3.6, stop bot.
