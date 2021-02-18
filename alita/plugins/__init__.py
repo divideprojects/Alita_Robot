be066da2600420e028f92dfa8684c74f61436589
Divkix <techdroidroot@gmail.com>
2021-02-18 05:05:00 +0000

    all_plugins -> all_plugs

--- alita/plugins/__init__.py
+++ alita/plugins/__init__.py
@@ -25,9 +25,9 @@ async def all_plugins():
     from os.path import basename, dirname, isfile
 
     mod_paths = glob(dirname(__file__) + "/*.py")
-    all_plugins = [
+    all_plugs = [
         basename(f)[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
-    return sorted(all_plugins)
+    return sorted(all_plugs)
