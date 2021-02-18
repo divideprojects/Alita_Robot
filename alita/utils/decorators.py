752d385caa848da26f4bf01c317b40d4cde07701
Divkix <techdroidroot@gmail.com>
2021-02-19 01:09:16 +0530

    Add custom decorators

---
+++ alita/utils/decorators.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from alita.bot_class import Alita
+from pyrogram.types import Message
+
+def user_admin(func):
+    @wraps(func)
+    async def decorator(bot: Alita, m: Message):
+        if bot.is_admin(m):
+            await func(bot, m)
+
+        await m.delete()
+
+    decorator.admin = True
+
+    return decorator
+
+
+def bot_admin(func):
+    @wraps(func)
+    async def decorator(bot: Alita, m: Message):
+        meh = (await bot.get_me()).id
+
+        if (await m.chat.get_member(meh)).status == "administrator":
+            await func(bot, m)
+
+        await m.delete()
+
+    decorator.admin = True
+
+    return decorator
