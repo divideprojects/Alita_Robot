90991908a385877bbfb1fbcf0c27214792fbd4fa
Divkix <techdroidroot@gmail.com>
2021-02-24 00:23:09 +0530

    [skip ci] Add Chat class

---
+++ alita/database/chats_db.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2020 - 2021 Divkix. All rights reserved. Source code available under the AGPL.
+#
+# This file is part of Alita_Robot.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from alita.database import MongoDB
+
+
+class Chats:
+    """Class to manage users for bot."""
+
+    def __init__(self) -> None:
+        self.collection = MongoDB("chats")
+
+    async def add_chat(self, chat_id: int, chat_name: str):
+        curr = (await self.collection.find_one({"chat_id": chat_id})))
+        if curr:
+            return True
+        return await self.collection.insert_one({"chat_id": chat_id, "chat_name": chat_name, "users": []})
+
+    async def update_user(self, user_id: int, chat_id: int, chat_name: str):
+        curr = (await self.collection.find_one({"chat_id": chat_id})))
+        if curr:
+            curr['users'].append(user_id)
+            return await self.collection.replace({"user_id": user_id}, {"user_id": user_id, "chat_id": chat_id, "chat_name": chat_name, "users": curr})
+        return await self.collection.insert_one({"user_id": user_id, "chat_id": chat_id, "chat_name": chat_name, "users": [])
+
+    async def delete_chat(self, chat_id: int):
+        curr = await self.collection.find_one({"chat_id": chat_id})
+        if curr:
+            return await self.collection.delete_one({"chat_id": chat_id})
+        return True
+
+    async def count_chats(self):
+        return await self.collection.count()
+
+    async def list_chats(self):
+        return await self.collection.find_all()
+
