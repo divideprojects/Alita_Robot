ef780d35676bfd861e0bc19b36f05930743fb86b
Divkix <23524935+Divkix@users.noreply.github.com>
2021-02-28 22:26:47 +0530

    first check if entities are there

--- alita/utils/extract_user.py
+++ alita/utils/extract_user.py
@@ -29,17 +29,18 @@ async def extract_user(c, m) -> Tuple[int, str]:
         user_first_name = m.reply_to_message.from_user.first_name
 
     elif len(m.command) > 1:
-        if len(m.entities) > 1:
-            required_entity = m.entities[1]
-            if required_entity.type == "text_mention":
-                user_id = required_entity.user.id
-                user_first_name = required_entity.user.first_name
-            elif required_entity.type == "mention":
-                user_id = m.text[
-                    required_entity.offset : required_entity.offset
-                    + required_entity.length
-                ]
-                user_first_name = user_id
+        if m.entities:
+            if len(m.entities) > 1:
+                required_entity = m.entities[1]
+                if required_entity.type == "text_mention":
+                    user_id = required_entity.user.id
+                    user_first_name = required_entity.user.first_name
+                elif required_entity.type == "mention":
+                    user_id = m.text[
+                        required_entity.offset : required_entity.offset
+                        + required_entity.length
+                    ]
+                    user_first_name = user_id
         else:
             user_id = m.command[1]
             user_first_name = user_id
