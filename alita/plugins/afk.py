78f0dbe9c7f5d694fdde2cc106adeed10e5b1f8d
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-01 07:45:24 +0530

    fix missing )

--- alita/plugins/afk.py
+++ alita/plugins/afk.py
@@ -95,7 +95,7 @@ async def afk_mentioned(c: Alita, m: Message):
         if not user_afk:
             return
 
-        since = strftime("%Hh %Mm %Ss", gmtime(time() - user_afk['time'])
+        since = strftime("%Hh %Mm %Ss", gmtime(time() - user_afk['time']))
         afkmsg = f"{user_first_name} is Afk!\n<b>Since:</b> <code>{since}</code>"
 
         if user_afk["reason"]:
@@ -118,7 +118,7 @@ async def rem_afk(c: Alita, m: Message):
         await m.reply_text(f"Error while checking afk\n{ef}")
         return
 
-    since = strftime("%Hh %Mm %Ss", gmtime(time() - user_afk['time'])
+    since = strftime("%Hh %Mm %Ss", gmtime(time() - user_afk['time']))
     await db.remove_afk(m.from_user.id)
     user = await c.get_users(user_afk['user_id'])
     await m.reply_text(
