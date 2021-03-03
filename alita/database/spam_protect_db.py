d400f42597c6e131534d3a38818e7f697d9a75f5
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-04 00:02:22 +0530

    tmp

---
+++ alita/database/spam_protect_db.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2020 - 2021 Divkix. All rights reserved. Source code available under the AGPL.
+#
+# This file is part of Alita_Robot.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from alita.database import MongoDB
+
+
+class SpamProtect:
+    """Class for managing Spam Protection settings of chats!"""
+
+    def __init__(self) -> None:
+        self.collection = MongoDB("spam_protect")
+
+    async def get_cas_status(self, chat_id: int):
+        curr = await self.collection.find_one({"chat_id": chat_id})
+        if curr:
+            stat = curr['cas']
+            return cas
+        return await self.collection.insert_one(
+            {"chat_id": chat_id, 'cas': False, 'attack': False},
+        )
+
+
+    async def add_afk(self, user_id: int, time: int, reason: str = ""):
+        if await self.check_afk(user_id):
+            return await self.collection.update(
+                {"user_id": user_id},
+                {"user_id": user_id, "reason": reason, "time": time},
+            )
+        return await self.collection.insert_one(
+            {"user_id": user_id, "reason": reason, "time": time},
+        )
+
+    async def remove_afk(self, user_id: int):
+        if await self.check_afk(user_id):
+            return await self.collection.delete_one({"user_id": user_id})
+        return
