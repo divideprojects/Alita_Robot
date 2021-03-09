053079e111eb5c2de637eb8bb4d86edd14899bc6
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-09 11:07:26 +0530

    Update tr_engine.py

--- alita/tr_engine/tr_engine.py
+++ alita/tr_engine/tr_engine.py
@@ -38,7 +38,7 @@ def cache_localizations(files):
     """Get all translated strings from files."""
     ldict = {lang: {} for lang in ENABLED_LOCALES}
     for file in files:
-        lang_name = (file.split(path.sep)[1]).replace(".yml", "")
+        lang_name = (file.split(path.sep)[1]).split("_")[0]
         lang_data = load_yml(open(file, encoding="utf-8"), Loader=FullLoader)
         ldict[lang_name] = lang_data
     return ldict
