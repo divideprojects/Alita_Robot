955fcc5028e545b8b91bdaf81a8db2a9d69ee501
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-11 00:18:32 +0530

    Update langs.py

--- alita/plugins/langs.py
+++ alita/plugins/langs.py
@@ -61,7 +61,7 @@ async def gen_langs_kb():
             )
             langs.pop(0)
         kb.append(a)
-    kb.append(InlineKeyboardButton("Help us with translations! 🌎", url="https://crowdin.com/project/alita_robot",))
+    kb.append([InlineKeyboardButton("Help us with translations! 🌎", url="https://crowdin.com/project/alita_robot")])
     return kb
 
 
