e5a8dd3ee3a846dc9a3baa33461f2e96da9e91a5
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-21 00:28:47 +0530

    add admincache reason

--- alita/plugins/admin.py
+++ alita/plugins/admin.py
@@ -106,7 +106,7 @@ async def reload_admins(_, m: Message):
             return
 
     try:
-        await admin_cache_reload(m)
+        await admin_cache_reload(m, "admincache")
         TEMP_ADMIN_CACHE_BLOCK[m.chat.id] = "manualblock"
         await m.reply_text(tlang(m, "admin.adminlist.reloaded_admins"))
         LOGGER.info(f"Admincache cmd use in {m.chat.id} by {m.from_user.id}")
