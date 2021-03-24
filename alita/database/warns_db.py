9dc8da438e1b19bcc53f1bea63a3ea41969395d1
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-24 14:52:14 +0530

    mend

---
+++ alita/database/warns_db.py
@@ -0,0 +1,153 @@
+# Copyright (C) 2020 - 2021 Divkix. All rights reserved. Source code available under the AGPL.
+#
+# This file is part of Alita_Robot.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from threading import RLock
+
+from alita.database import MongoDB
+
+INSERTION_LOCK = RLock()
+
+WARNS_CACHE = {}
+
+
+class Warns:
+    def __init__(self) -> None:
+        self.collection = MongoDB("chat_warns")
+
+    def warn_user(self, chat_id: int, user_id: int, warn_reason=None):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"chat_id": chat_id, "user_id": user_id})
+            if curr:
+                curr_warns = curr["warns"] + [warn_reason]
+                num_warns = curr["num_warns"] + 1
+                self.collection.update(
+                    {"chat_id": chat_id, "user_id": user_id},
+                    {"warns": curr_warns, "num_warns": num_warns},
+                )
+                return curr_warns, num_warns
+
+            self.collection.insert_one(
+                {
+                    "chat_id": chat_id,
+                    "user_id": user_id,
+                    "warns": [warn_reason],
+                    "num_warns": 1,
+                },
+            )
+
+            return [warn_reason], 1
+
+    def remove_warn(self, chat_id: int, user_id: int):
+        with INSERTION_LOCK:
+
+            curr = self.collection.find_one({"chat_id": chat_id, "user_id": user_id})
+            if curr:
+                curr_warns = curr["warns"][:-1]
+                num_warns = curr["num_warns"] - 1
+                self.collection.update(
+                    {"chat_id": chat_id, "user_id": user_id},
+                    {"warns": curr_warns, "num_warns": num_warns},
+                )
+                return curr_warns, num_warns
+
+            return False, False
+
+    def reset_warns(self, chat_id: int, user_id: int):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"chat_id": chat_id, "user_id": user_id})
+            if curr:
+                return self.collection.delete_one(
+                    {"chat_id": chat_id, "user_id": user_id},
+                )
+            return True
+
+    def get_warns(self, chat_id: int, user_id: int):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"chat_id": chat_id, "user_id": user_id})
+            if curr:
+                return curr["warns"], curr["num_warns"]
+            return [], 0
+
+
+class WarnSettings:
+    def __init__(self) -> None:
+        self.collection = MongoDB("chat_warn_settings")
+
+    def get_warnings_settings(self, chat_id: int):
+        curr = self.collection.find_one({"_id": chat_id})
+        if curr:
+            return curr
+        curr = {"_id": chat_id, "warn_mode": "kick", "warn_limit": 3}
+        self.collection.insert_one(curr)
+        return curr
+
+    def set_warnmode(self, chat_id: int, warn_mode: str = "kick"):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"_id": chat_id})
+            if curr:
+                self.collection.update(
+                    {"_id": chat_id},
+                    {"warn_mode": warn_mode},
+                )
+                return warn_mode
+
+            self.collection.insert_one(
+                {"_id": chat_id, "warn_mode": warn_mode, "warn_limit": 3},
+            )
+
+            return warn_mode
+
+    def get_warnmode(self, chat_id: int):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"_id": chat_id})
+            if curr:
+                return curr
+
+            self.collection.insert_one(
+                {"_id": chat_id, "warn_mode": "kick", "warn_limit": 3},
+            )
+
+            return "kick"
+
+    def set_warnlimit(self, chat_id: int, warn_limit: int = 3):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"_id": chat_id})
+            if curr:
+                self.collection.update(
+                    {"_id": chat_id},
+                    {"warn_limit": warn_limit},
+                )
+                return warn_limit
+
+            self.collection.insert_one(
+                {"_id": chat_id, "warn_mode": "kick", "warn_limit": warn_limit},
+            )
+
+            return warn_limit
+
+    def get_warnlimit(self, chat_id: int):
+        with INSERTION_LOCK:
+            curr = self.collection.find_one({"_id": chat_id})
+            if curr:
+                return curr["warn_limit"]
+
+            self.collection.insert_one(
+                {"_id": chat_id, "warn_mode": "kick", "warn_limit": 3},
+            )
+
+            return 3
---
+++ alita/plugins/warns.py
@@ -0,0 +1,314 @@
+# Copyright (C) 2020 - 2021 Divkix. All rights reserved. Source code available under the AGPL.
+#
+# This file is part of Alita_Robot.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from time import time
+
+from pyrogram import filters
+from pyrogram.types import (
+    CallbackQuery,
+    ChatPermissions,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+
+from alita import LOGGER, PREFIX_HANDLER, SUPPORT_STAFF
+from alita.bot_class import Alita
+from alita.database.rules_db import Rules
+from alita.database.warns_db import Warns, WarnSettings
+from alita.tr_engine import tlang
+from alita.utils.admin_cache import ADMIN_CACHE, admin_cache_reload
+from alita.utils.custom_filters import admin_filter, restrict_filter
+from alita.utils.extract_user import extract_user
+from alita.utils.parser import mention_html
+
+db = Warns()
+rules_db = Rules()
+warn_sdb = WarnSettings()
+
+
+@Alita.on_message(filters.command("warn", PREFIX_HANDLER) & restrict_filter)
+async def warn(c: Alita, m: Message):
+    from alita import BOT_USERNAME
+
+    if not len(m.command) > 1 and not m.reply_to_message:
+        await m.reply_text("I can't warn nothing! Tell me user whom I should warn")
+        return
+
+    user_id, user_first_name, _ = await extract_user(c, m)
+
+    if user_id in SUPPORT_STAFF:
+        await m.reply_text(tlang(m, "admin.support_cannot_restrict"))
+        LOGGER.info(
+            f"{m.from_user.id} trying to warn {user_id} (SUPPORT_STAFF) in {m.chat.id}",
+        )
+        return
+
+    try:
+        admins_group = {i[0] for i in ADMIN_CACHE[m.chat.id]}
+    except KeyError:
+        admins_group = await admin_cache_reload(m, "warn")
+
+    if user_id in admins_group:
+        await m.reply_text("This user is admin in this chat, I can't warn them!")
+        return
+
+    _, num = db.warn_user(m.chat.id, user_id)
+    warn_settings = warn_sdb.get_warnings_settings(m.chat.id)
+    if num >= warn_settings["warn_limit"]:
+        if warn_settings["warn_mode"] == "kick":
+            await m.chat.kick_member(user_id, until_date=(time() + 15))
+            action = "kicked"
+        elif warn_settings["warn_mode"] == "ban":
+            await m.chat.kick_member(user_id)
+            action = "banned"
+        elif warn_settings["warn_mode"] == "mute":
+            await m.chat.restrict_member(user_id, ChatPermissions())
+            action = "muted"
+        await m.reply_text(
+            (
+                f"Warnings {num}/{warn_settings['warn_limit']}\n"
+                f"{(await mention_html(user_first_name, user_id))} has been <b>{action}</b>!"
+            ),
+        )
+        return
+
+    rules = rules_db.get_rules(m.chat.id)
+    if rules:
+        kb = [
+            InlineKeyboardButton(
+                "Remove Warn!",
+                callback_data=f"remove_warn.{user_id}",
+            ),
+            InlineKeyboardButton(
+                "Rules",
+                url=f"https://t.me/{BOT_USERNAME}?start=rules_{m.chat.id}",
+            ),
+        ]
+    else:
+        kb = [
+            InlineKeyboardButton(
+                "Remove Warn!",
+                callback_data=f"remove_warn.{user_id}",
+            ),
+        ]
+
+    await m.reply_text(
+        f"{(await mention_html(user_first_name, user_id))} has {num} warnings!",
+        reply_markup=InlineKeyboardMarkup([kb]),
+    )
+    return
+
+
+@Alita.on_message(filters.command("resetwarns", PREFIX_HANDLER) & restrict_filter)
+async def reset_warn(c: Alita, m: Message):
+
+    if not len(m.command) > 1 and not m.reply_to_message:
+        await m.reply_text("I can't warn nothing! Tell me user whom I should warn")
+        return
+
+    user_id, user_first_name, _ = await extract_user(c, m)
+
+    if user_id in SUPPORT_STAFF:
+        await m.reply_text(
+            "They are support users, cannot be restriced, how am I then supposed to unrestrict them?",
+        )
+        LOGGER.info(
+            f"{m.from_user.id} trying to resetwarn {user_id} (SUPPORT_STAFF) in {m.chat.id}",
+        )
+        return
+
+    try:
+        admins_group = {i[0] for i in ADMIN_CACHE[m.chat.id]}
+    except KeyError:
+        admins_group = await admin_cache_reload(m, "warn")
+
+    if user_id in admins_group:
+        await m.reply_text("This user is admin in this chat, I can't warn them!")
+        return
+
+    db.reset_warns(m.chat.id, user_id)
+    await m.reply_text(
+        f"Warnings have been reset for {(await mention_html(user_first_name,user_id))}",
+    )
+    return
+
+
+@Alita.on_message(filters.command("warns", PREFIX_HANDLER) & restrict_filter)
+async def list_warns(c: Alita, m: Message):
+
+    if not len(m.command) > 1 and not m.reply_to_message:
+        await m.reply_text("I can't warn nothing! Tell me user whom I should warn")
+        return
+
+    user_id, user_first_name, _ = await extract_user(c, m)
+
+    if user_id in SUPPORT_STAFF:
+        await m.reply_text("This user has no warns!")
+        LOGGER.info(
+            f"{m.from_user.id} trying to check warns of {user_id} (SUPPORT_STAFF) in {m.chat.id}",
+        )
+        return
+
+    try:
+        admins_group = {i[0] for i in ADMIN_CACHE[m.chat.id]}
+    except KeyError:
+        admins_group = await admin_cache_reload(m, "list_warns")
+
+    if user_id in admins_group:
+        await m.reply_text(
+            "This user is admin in this chat, they don't have any warns!",
+        )
+        return
+
+    warns, num_warns = db.get_warns(m.chat.id, user_id)
+    msg = f"{(await mention_html(user_first_name,user_id))} has <b>{num_warns}</b> warns!\n\n<b>Reasons:</b>\n"
+    msg += "\n".join([f"- {i}" for i in warns])
+    await m.reply_text(msg)
+    return
+
+
+@Alita.on_message(filters.command("rmwarn", PREFIX_HANDLER) & restrict_filter)
+async def remove_warn(c: Alita, m: Message):
+
+    if not len(m.command) > 1 and not m.reply_to_message:
+        await m.reply_text(
+            "I can't remove warns of nothing! Tell me user whose warn should be removed!",
+        )
+        return
+
+    user_id, user_first_name, _ = await extract_user(c, m)
+
+    if user_id in SUPPORT_STAFF:
+        await m.reply_text("This user has no warns!")
+        LOGGER.info(
+            f"{m.from_user.id} trying to remove warns of {user_id} (SUPPORT_STAFF) in {m.chat.id}",
+        )
+        return
+
+    try:
+        admins_group = {i[0] for i in ADMIN_CACHE[m.chat.id]}
+    except KeyError:
+        admins_group = await admin_cache_reload(m, "list_warns")
+
+    if user_id in admins_group:
+        await m.reply_text(
+            "This user is admin in this chat, they don't have any warns!",
+        )
+        return
+
+    warns, _ = db.get_warns(m.chat.id, user_id)
+    if not warns:
+        await m.reply_text("This user has no warnings!")
+        return
+
+    _, num_warns = db.remove_warn(m.chat.id, user_id)
+    await m.reply_text(
+        (
+            f"{(await mention_html(user_first_name,user_id))} now has <b>{num_warns}</b> warnings!\n"
+            "Their last warn was removed."
+        ),
+    )
+    return
+
+
+@Alita.on_callback_query(filters.regex("^remove_warn."))
+async def remove_last_warn_btn(_, q: CallbackQuery):
+
+    try:
+        admins_group = {i[0] for i in ADMIN_CACHE[q.message.chat.id]}
+    except KeyError:
+        admins_group = {
+            i[0] for i in (await admin_cache_reload(q, "warn_remove_button"))
+        }
+
+    if not q.from_user.id in admins_group:
+        await q.answer("You are not allowed to use this!", show_alert=True)
+        return
+
+    args = q.data.split(".")
+    user_id = int(args[1])
+    chat_id = int(q.message.chat.id)
+
+    _, num_warns = db.remove_warn(chat_id, user_id)
+
+    if not num_warns:
+        num_warns = 0
+
+    await q.message.edit_text(
+        (
+            f"Warn removed by {(await mention_html(q.from_user.first_name,q.from_user.id))}\n"
+            f"<b>Current warnings:</b> {num_warns}"
+        ),
+    )
+
+    await q.answer()
+    return
+
+
+@Alita.on_message(filters.command("warnings", PREFIX_HANDLER) & admin_filter)
+async def get_settings(_, m: Message):
+    settings = warn_sdb.get_warnings_settings(m.chat.id)
+    await m.reply_text(
+        (
+            "This group has these following settings:\n"
+            f"Warn Limit: {settings['warn_limit']}\n"
+            f"Warn Mode: {settings['warn_mode']}"
+        ),
+    )
+    return
+
+
+@Alita.on_message(filters.command("warnmode", PREFIX_HANDLER) & admin_filter)
+async def warnmode(_, m: Message):
+    if len(m.command) > 1:
+        wm = m.command[1]
+        if wm not in ("kick", "ban", "mute"):
+            await m.reply_text(
+                (
+                    "Please choose a valid warn mode!"
+                    "Valid options are: <code>ban</code>,<code>kick</code>,<code>mute</code>"
+                ),
+            )
+            return
+        warnmode = warn_sdb.set_warnmode(m.chat.id, wm)
+        await m.reply_text(f"Warn Mode has been set to: {warnmode}")
+        return
+    warnmode = warn_sdb.get_warnmode(m.chat.id)
+    await m.reply_text(f"This chats current Warn Mode is: {warnmode}")
+    return
+
+
+@Alita.on_message(filters.command("warnlimit", PREFIX_HANDLER) & admin_filter)
+async def warnlimit(_, m: Message):
+    if len(m.command) > 1:
+        wl = m.command[1]
+        if not isinstance(wl, int):
+            await m.reply_text("Warn Limit can only be a number!")
+            return
+        warnlimit = warn_sdb.set_warnlimit(m.chat.id, wl)
+        await m.reply_text(f" Warn Limit has been set to: {warnlimit}")
+        return
+    warnlimit = warn_sdb.get_warnlimit(m.chat.id)
+    await m.reply_text(f"This chats current Warn Limit is: {warnlimit}")
+    return
+
+
+# __PLUGIN__ = "plugins.warnings.main"
+# __help__ = "plugins.warnings.help"
+# __alt_name__ = ["warn", "warning"]
