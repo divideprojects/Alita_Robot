7aa1ab705db43ebce8f9f72a64446834da7a119a
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-25 08:05:25 +0530

    filter only first word or if separated by '|'

--- alita/plugins/filters.py
+++ alita/plugins/filters.py
@@ -97,7 +97,11 @@ async def add_filter(_, m: Message):
                 "Please provide keyword for this filter to reply with!",
             )
             return
-        keyword = args[1].lower()
+        if '|' in (args[1].split()[1]).lower():
+            extracted = await split_quotes((args[1].split()[1]).lower())
+            keyword = extracted[0].lower()
+         else:
+             keyword = (args[1].split()[1]).lower()
     else:
         extracted = await split_quotes(args[1])
         keyword = extracted[0].lower()
