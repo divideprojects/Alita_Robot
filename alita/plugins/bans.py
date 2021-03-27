6cd267c459995de42646d2036c18dc24a5872497
Divkix <23524935+Divkix@users.noreply.github.com>
2021-03-27 21:01:53 +0530

    reply_to_message_id added

--- alita/plugins/bans.py
+++ alita/plugins/bans.py
@@ -47,10 +47,14 @@ async def kick_usr(c: Alita, m: Message):
         await m.reply_text(tlang(m, "admin.kick.no_target"))
         await m.stop_propagation()
 
-    if m.reply_to_message and len(m.text.split()) >= 2:
-        reason = m.text.split(None, 1)[1]
-    elif not m.reply_to_message and len(m.text.split()) >= 3:
-        reason = m.text.split(None, 2)[2]
+    if m.reply_to_message:
+        r_id = m.reply_to_message.message_id
+        if len(m.text.split()) >= 2:
+            reason = m.text.split(None, 1)[1]
+    elif not m.reply_to_message:
+        r_id = m.message_id
+        if len(m.text.split()) >= 3:
+            reason = m.text.split(None, 2)[2]
     else:
         reason = None
 
@@ -93,7 +97,7 @@ async def kick_usr(c: Alita, m: Message):
             chat_title=m.chat.title,
         )
         txt += f"\n<b>Reason</b>: {reason}" if reason else ""
-        await m.reply_text(txt)
+        await m.reply_text(txt, reply_to_message_id=r_id)
     except ChatAdminRequired:
         await m.reply_text(tlang(m, "admin.not_admin"))
     except PeerIdInvalid:
@@ -127,10 +131,14 @@ async def ban_usr(c: Alita, m: Message):
         await m.reply_text(tlang(m, "admin.ban.no_target"))
         await m.stop_propagation()
 
-    if m.reply_to_message and len(m.text.split()) >= 2:
-        reason = m.text.split(None, 1)[1]
-    elif not m.reply_to_message and len(m.text.split()) >= 3:
-        reason = m.text.split(None, 2)[2]
+    if m.reply_to_message:
+        r_id = m.reply_to_message.message_id
+        if len(m.text.split()) >= 2:
+            reason = m.text.split(None, 1)[1]
+    elif not m.reply_to_message:
+        r_id = m.message_id
+        if len(m.text.split()) >= 3:
+            reason = m.text.split(None, 2)[2]
     else:
         reason = None
 
@@ -173,7 +181,7 @@ async def ban_usr(c: Alita, m: Message):
             chat_title=m.chat.title,
         )
         txt += f"\n<b>Reason</b>: {reason}" if reason else ""
-        await m.reply_text(txt)
+        await m.reply_text(txt, reply_to_message_id=r_id)
     except ChatAdminRequired:
         await m.reply_text(tlang(m, "admin.not_admin"))
     except PeerIdInvalid:
