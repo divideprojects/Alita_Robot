68101abd687934e24a41b38688560deff4ed2d6a
Divkix <23524935+Divkix@users.noreply.github.com>
2021-04-22 22:13:07 +0530

    Update watchers.py

--- alita/plugins/watchers.py
+++ alita/plugins/watchers.py
@@ -157,8 +157,8 @@ async def bl_watcher(_, m: Message):
             await m.reply_text(
                 (
                     f"{(await mention_html(m.from_user.first_name, m.from_user.id))} warned {num}/{warn_settings['warn_limit']}\n"
-                    f"Last warn was for:\n<i>{warn_reason}</i>"
-                    # f"Last warn was for:\n<i>{warn_reason.format(trigger)}</i>"
+                    # f"Last warn was for:\n<i>{warn_reason}</i>"
+                    f"Last warn was for:\n<i>{warn_reason.format(trigger)}</i>"
                 ),
             )
         else:
