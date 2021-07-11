4f1bfd73bc55b59f4114f5328faf807af7dee79a
ÁÑÑÍHÌLÅTØR SPÄRK <tanmoyomg7@gmail.com>
2021-07-11 23:51:40 +0530

    added tmute dtmute stmute

--- alita/plugins/muting.py
+++ alita/plugins/muting.py
@@ -31,11 +31,121 @@ from alita.utils.caching import ADMIN_CACHE, admin_cache_reload
 from alita.utils.custom_filters import command, restrict_filter
 from alita.utils.extract_user import extract_user
 from alita.utils.parser import mention_html
+from alita.utils.string import extract_time
+
+
+@Alita.on_message(command(["tmute", "stmute", "dtmute"]) & restrict_filter)
+async def mute_usr(c: Alita, m: Message):
+    if len(m.text.split()) == 1 and not m.reply_to_message:
+        await m.reply_text("I can't mute nothing!")
+        return
+
+    user_id, user_first_name, _ = await extract_user(c, m)
+
+    if not user_id:
+        await m.reply_text("Cannot find user to mute")
+        return
+    if user_id == BOT_ID:
+        await m.reply_text("Huh, why would I mute myself?")
+        return
+
+    if user_id in SUPPORT_STAFF:
+        LOGGER.info(
+            f"{m.from_user.id} trying to mute {user_id} (SUPPORT_STAFF) in {m.chat.id}",
+        )
+        await m.reply_text(tlang(m, "admin.support_cannot_restrict"))
+        return
+
+    try:
+        admins_group = {i[0] for i in ADMIN_CACHE[m.chat.id]}
+    except KeyError:
+        admins_group = await admin_cache_reload(m, "mute")
+
+    if user_id in admins_group:
+        await m.reply_text(tlang(m, "admin.mute.admin_cannot_mute"))
+        return
+
+    if m.reply_to_message:
+        r_id = m.reply_to_message.message_id
+        if len(m.text.split()) >= 2:
+            reason = m.text.split(None, 1)[1]
+    else:
+        r_id = m.message_id
+        if len(m.text.split()) >= 3:
+            reason = m.text.split(None, 2)[2]
+
+    if not reason:
+        await m.reply_text("You haven't specified a time to mute this user for!")
+        return
+
+    split_reason = reason.split(None, 1)
+    time_val = split_reason[0].lower()
+    if len(split_reason) > 1:
+        reason = split_reason[1]
+    else:
+        reason = ""
+
+    mutetime = extract_time(message, time_val)
+
+    if not mutetime:
+        return
+
+    try:
+        await m.chat.restrict_member(
+            user_id,
+            ChatPermissions(
+                can_send_messages=False,
+                can_send_media_messages=False,
+                can_send_stickers=False,
+                can_send_animations=False,
+                can_send_games=False,
+                can_use_inline_bots=False,
+                can_add_web_page_previews=False,
+                can_send_polls=False,
+                can_change_info=False,
+                can_invite_users=True,
+                can_pin_messages=False,
+            ),
+            mutetime,
+        )
+        LOGGER.info(f"{m.from_user.id} muted {user_id} in {m.chat.id}")
+        if m.text.split()[0] == "/stmute":
+            await m.delete()
+            if m.reply_to_message:
+                await m.reply_to_message.delete()
+            await m.stop_propagation()
+        if m.text.split()[0] == "/dtmute":
+            if not m.reply_to_message:
+                await m.reply_text("Reply to a message to delete it and mute the user!")
+                await m.stop_propagation()
+            await m.reply_to_message.delete()
+        txt = (tlang(m, "admin.mute.muted_user")).format(
+            admin=(await mention_html(m.from_user.first_name, m.from_user.id)),
+            muted=(await mention_html(user_first_name, user_id)),
+        )
+        if reason:
+            txt += f"\n<b>Reason</b>: {reason}"
+        await m.reply_text(txt, reply_to_message_id=r_id)
+    except ChatAdminRequired:
+        await m.reply_text(tlang(m, "admin.not_admin"))
+    except RightForbidden:
+        await m.reply_text(tlang(m, "admin.mute.bot_no_right"))
+    except UserNotParticipant:
+        await m.reply_text("How can I mute a user who is not a part of this chat?")
+    except RPCError as ef:
+        await m.reply_text(
+            (tlang(m, "general.some_error")).format(
+                SUPPORT_GROUP=SUPPORT_GROUP,
+                ef=ef,
+            ),
+        )
+        LOGGER.error(ef)
+
+    return
 
 
 @Alita.on_message(command(["mute", "smute", "dmute"]) & restrict_filter)
 async def mute_usr(c: Alita, m: Message):
-
     if len(m.text.split()) == 1 and not m.reply_to_message:
         await m.reply_text("I can't mute nothing!")
         return
@@ -129,7 +239,6 @@ async def mute_usr(c: Alita, m: Message):
 
 @Alita.on_message(command("unmute") & restrict_filter)
 async def unmute_usr(c: Alita, m: Message):
-
     if len(m.text.split()) == 1 and not m.reply_to_message:
         await m.reply_text("I can't unmute nothing!")
         return
@@ -168,4 +277,4 @@ async def unmute_usr(c: Alita, m: Message):
 
 __PLUGIN__ = "muting"
 
-__alt_name__ = ["mute", "unmute"]
+__alt_name__ = ["mute", "tmute", "unmute", ]
