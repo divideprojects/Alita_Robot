7674d975704e1fac611f051828f5bd90cb4bf0c0
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2021-10-04 11:02:53 +0530

    deploy fix only

--- alita/database/group_blacklist.py
+++ alita/database/group_blacklist.py
@@ -22,6 +22,7 @@ from alita.database import MongoDB
 from alita.database.chats_db import Chats
 
 INSERTION_LOCK = RLock()
+BLACKLIST_CHATS = []
 
 
 class GroupBlacklist(MongoDB):
@@ -34,17 +35,30 @@ class GroupBlacklist(MongoDB):
 
     def add_chat(self, chat_id: int):
         with INSERTION_LOCK:
-            Chats.remove_chat(chat_id)  # Delete chat from database
+            global BLACKLIST_CHATS
+            try:
+                Chats.remove_chat(chat_id)  # Delete chat from database
+            except KeyError:
+                pass
+            BLACKLIST_CHATS.append(chat_id)
+            BLACKLIST_CHATS.sort()
             return self.insert_one({"_id": chat_id, "blacklist": True})
 
     def remove_chat(self, chat_id: int):
         with INSERTION_LOCK:
+            global BLACKLIST_CHATS
+            BLACKLIST_CHATS.remove(chat_id)
+            BLACKLIST_CHATS.sort()
             return self.delete_one({"_id": chat_id})
 
     def list_all_chats(self):
         with INSERTION_LOCK:
-            all_chats = self.find_all()
-            return [chat["_id"] for chat in all_chats]
+            try:
+                BLACKLIST_CHATS.sort()
+                return BLACKLIST_CHATS
+            except Exception:
+                all_chats = self.find_all()
+                return [chat["_id"] for chat in all_chats]
 
     def get_from_db(self):
         return self.find_all()
