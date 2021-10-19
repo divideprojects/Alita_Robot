e9fc83e24f36be065e6fbe16e5359766ad2e9eb0
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2021-10-19 14:29:02 +0530

    Update fun.py

--- alita/plugins/fun.py
+++ alita/plugins/fun.py
@@ -15,12 +15,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from html import escape
 from secrets import choice
 
+from pyrogram.errors import MessageTooLong
 from pyrogram.types import Message
 
+from alita import LOGGER
 from alita.bot_class import Alita
 from alita.tr_engine import tlang
 from alita.utils import fun_strings
@@ -36,21 +37,27 @@ async def fun_shout(_, m: Message):
             quote=True,
         )
         return
-    text = " ".join(m.text.split(None, 1)[1])
-    result = [" ".join(list(text))]
-    for pos, symbol in enumerate(text[1:]):
-        result.append(symbol + " " + "  " * pos + symbol)
-    result = list("\n".join(result))
-    result[0] = text[0]
-    result = "".join(result)
-    msg = "```\n" + result + "```"
-    await m.reply_text(msg, parse_mode="markdown")
-    return
+    try:
+        text = " ".join(m.text.split(None, 1)[1])
+        result = [" ".join(list(text))]
+        for pos, symbol in enumerate(text[1:]):
+            result.append(symbol + " " + "  " * pos + symbol)
+        result = list("\n".join(result))
+        result[0] = text[0]
+        result = "".join(result)
+        msg = "```\n" + result + "```"
+        await m.reply_text(msg, parse_mode="markdown")
+        LOGGER.info(f"{m.from_user.id} shouted in {m.chat.id}")
+        return
+    except MessageTooLong as e:
+        await m.reply_text(f"Error: {e}")
+        return
 
 
 @Alita.on_message(command("runs"))
 async def fun_run(_, m: Message):
     await m.reply_text(choice(fun_strings.RUN_STRINGS))
+    LOGGER.info(f"{m.from_user.id} runed in {m.chat.id}")
     return
 
 
@@ -61,7 +68,10 @@ async def fun_slap(c: Alita, m: Message):
     reply_text = m.reply_to_message.reply_text if m.reply_to_message else m.reply_text
 
     curr_user = escape(m.from_user.first_name)
-    user_id, _, _ = await extract_user(c, m)
+    try:
+        user_id, user_first_name, _ = await extract_user(c, m)
+    except Exception:
+        return
 
     if user_id == me.id:
         temp = choice(fun_strings.SLAP_ALITA_TEMPLATES)
@@ -69,9 +79,8 @@ async def fun_slap(c: Alita, m: Message):
         temp = choice(fun_strings.SLAP_TEMPLATES)
 
     if user_id:
-        slapped_user = await c.get_users(user_id)
         user1 = curr_user
-        user2 = escape(slapped_user.first_name)
+        user2 = escape(user_first_name)
 
     else:
         user1 = me.first_name
@@ -81,9 +90,13 @@ async def fun_slap(c: Alita, m: Message):
     hit = choice(fun_strings.HIT)
     throw = choice(fun_strings.THROW)
 
-    reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
-
+    reply = temp.format(user1=user1,
+                        user2=user2,
+                        item=item,
+                        hits=hit,
+                        throws=throw)
     await reply_text(reply)
+    LOGGER.info(f"{m.from_user.id} slaped in {m.chat.id}")
     return
 
 
@@ -91,6 +104,7 @@ async def fun_slap(c: Alita, m: Message):
 async def fun_roll(_, m: Message):
     reply_text = m.reply_to_message.reply_text if m.reply_to_message else m.reply_text
     await reply_text(choice(range(1, 7)))
+    LOGGER.info(f"{m.from_user.id} roll in {m.chat.id}")
     return
 
 
@@ -98,6 +112,7 @@ async def fun_roll(_, m: Message):
 async def fun_toss(_, m: Message):
     reply_text = m.reply_to_message.reply_text if m.reply_to_message else m.reply_text
     await reply_text(choice(fun_strings.TOSS))
+    LOGGER.info(f"{m.from_user.id} tossed in {m.chat.id}")
     return
 
 
@@ -105,6 +120,7 @@ async def fun_toss(_, m: Message):
 async def fun_shrug(_, m: Message):
     reply_text = m.reply_to_message.reply_text if m.reply_to_message else m.reply_text
     await reply_text(r"¯\_(ツ)_/¯")
+    LOGGER.info(f"{m.from_user.id} shruged in {m.chat.id}")
     return
 
 
@@ -114,6 +130,7 @@ async def fun_bluetext(_, m: Message):
     await reply_text(
         "/BLUE /TEXT\n/MUST /CLICK\n/I /AM /A /STUPID /ANIMAL /THAT /IS /ATTRACTED /TO /COLORS",
     )
+    LOGGER.info(f"{m.from_user.id} bluetexted in {m.chat.id}")
     return
 
 
@@ -121,6 +138,7 @@ async def fun_bluetext(_, m: Message):
 async def fun_decide(_, m: Message):
     reply_text = m.reply_to_message.reply_text if m.reply_to_message else m.reply_text
     await reply_text(choice(fun_strings.DECIDE))
+    LOGGER.info(f"{m.from_user.id} decided in {m.chat.id}")
     return
 
 
@@ -128,7 +146,53 @@ async def fun_decide(_, m: Message):
 async def fun_table(_, m: Message):
     reply_text = m.reply_to_message.reply_text if m.reply_to_message else m.reply_text
     await reply_text(choice(fun_strings.REACTIONS))
+    LOGGER.info(f"{m.from_user.id} reacted in {m.chat.id}")
+    return
+
+
+@Alita.on_message(command("weebify"))
+async def weebify(_, m: Message):
+    if len(m.text.split()) >= 2:
+        args = m.text.split(None, 1)[1]
+    elif m.reply_to_message and len(m.text.split()) == 1:
+        args = m.reply_to_message.text
+    else:
+        await m.reply_text(
+            "Please reply to a message or enter text after command to weebify it.",
+        )
+        return
+    if not args:
+        await m.reply_text(tlang(m, "utils.weebify.weebify_what"))
+        return
+
+    # Use split to convert to list
+    # Not using list itself becuase black changes it to long format...
+    normiefont = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+    weebyfont = "卂 乃 匚 刀 乇 下 厶 卄 工 丁 长 乚 从 𠘨 口 尸 㔿 尺 丂 丅 凵 リ 山 乂 丫 乙".split()
+
+    string = "  ".join(args).lower()
+    for normiecharacter in string:
+        if normiecharacter in normiefont:
+            weebycharacter = weebyfont[normiefont.index(normiecharacter)]
+            string = string.replace(normiecharacter, weebycharacter)
+
+    await m.reply_text(
+        (tlang(m, "utils.weebify.weebified_string").format(string=string)), )
+    LOGGER.info(f"{m.from_user.id} weebified '{args}' in {m.chat.id}")
     return
 
 
 __PLUGIN__ = "fun"
+
+_DISABLE_CMDS_ = [
+    "weebify",
+    "decide",
+    "react",
+    "bluetext",
+    "toss",
+    "roll",
+    "slap",
+    "runs",
+    "shout",
+    "shrug",
+]
