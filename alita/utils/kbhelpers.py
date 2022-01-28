89060f739556cc2c8e4e1e174c19a05d17239d69
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-01-27 21:07:45 -0700

    Update LICENSE
    Remove header LICENSE from all project files and update LICENSE for year 2022

--- alita/utils/kbhelpers.py
+++ alita/utils/kbhelpers.py
@@ -1,22 +1,3 @@
-# Copyright (C) 2020 - 2021 Divkix. All rights reserved. Source code available under the AGPL.
-#
-# This file is part of Alita_Robot.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# The following snipplet has been taken from:
-# https://github.com/usernein/pyromod/blob/86d9d7cae2e0c4bf144bf054461d0bd980755138/pyromod/helpers/helpers.py#L3-#L15
-
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
