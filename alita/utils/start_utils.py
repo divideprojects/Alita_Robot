e1557cc8ce7d673acf2ae47a1fdc6cec6c6d4c8c
annihilatorrrr <tanmoyomg7@gmail.com>
2022-06-03 18:32:15 +0530

    some more upgrades

--- alita/utils/start_utils.py
+++ alita/utils/start_utils.py
@@ -1,9 +1,14 @@
 from html import escape
 from secrets import choice
-from traceback import format_exc
 
+from pyrogram.enums import ParseMode
 from pyrogram.errors import RPCError
-from pyrogram.types import CallbackQuery, Message
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 from alita import HELP_COMMANDS, LOGGER, SUPPORT_GROUP
 from alita.bot_class import Alita
@@ -12,17 +17,12 @@ from alita.database.notes_db import Notes
 from alita.database.rules_db import Rules
 from alita.tr_engine import tlang
 from alita.utils.cmd_senders import send_cmd
-from alita.utils.kbhelpers import ikb
 from alita.utils.msg_types import Types
 from alita.utils.string import (
     build_keyboard,
     escape_mentions_using_curly_brackets,
     parse_button,
 )
-from alita.vars import Config
-
-# Initialize
-notes_db = Notes()
 
 
 async def gen_cmds_kb(m: Message or CallbackQuery):
@@ -31,34 +31,34 @@ async def gen_cmds_kb(m: Message or CallbackQuery):
         m = m.message
 
     cmds = sorted(list(HELP_COMMANDS.keys()))
-    kb = [(tlang(m, cmd), f"get_mod.{cmd.lower()}") for cmd in cmds]
+    kb = [
+        InlineKeyboardButton(tlang(m, cmd), callback_data=f"get_mod.{cmd.lower()}")
+        for cmd in cmds
+    ]
 
-    return [kb[i : i + 3] for i in range(0, len(kb), 3)]
+    return [kb[i: i + 3] for i in range(0, len(kb), 3)]
 
 
 async def gen_start_kb(q: Message or CallbackQuery):
     """Generate keyboard with start menu options."""
-    return ikb(
+    from alita.vars import Config
+
+    return InlineKeyboardMarkup(
         [
             [
-                (
+                InlineKeyboardButton(
                     f"➕ {(tlang(q, 'start.add_chat_btn'))}",
-                    f"https://t.me/{Config.BOT_USERNAME}?startgroup=new",
-                    "url",
+                    url=f"https://t.me/{Config.BOT_USERNAME}?startgroup=new",
                 ),
-                (
+                InlineKeyboardButton(
                     f"{(tlang(q, 'start.support_group'))} 👥",
-                    f"https://t.me/{SUPPORT_GROUP}",
-                    "url",
+                    url=f"https://t.me/{SUPPORT_GROUP}",
                 ),
             ],
-            [(f"📚 {(tlang(q, 'start.commands_btn'))}", "commands")],
             [
-                (f"🌐 {(tlang(q, 'start.language_btn'))}", "chlang"),
-                (
-                    f"🗃️ {(tlang(q, 'start.source_code'))}",
-                    "https://github.com/DivideProjects/Alita_Robot",
-                    "url",
+                InlineKeyboardButton(
+                    f"📚 {(tlang(q, 'start.commands_btn'))}",
+                    callback_data="commands",
                 ),
             ],
         ],
@@ -67,32 +67,34 @@ async def gen_start_kb(q: Message or CallbackQuery):
 
 async def get_private_note(c: Alita, m: Message, help_option: str):
     """Get the note in pm of user, with parsing enabled."""
+    from alita.vars import Config
+    # Initialize
+    notes_db = Notes()
+
     help_lst = help_option.split("_")
     if len(help_lst) == 2:
         chat_id = int(help_lst[1])
 
         all_notes = notes_db.get_all_notes(chat_id)
         chat_title = Chats.get_chat_info(chat_id)["chat_name"]
-        rply = f"Notes in {chat_title}:\n\n"
+        rply = f"Notes in {chat_title}:\n"
         note_list = [
-            f"- [{note[0]}](https://t.me/{Config.BOT_USERNAME}?start=note_{chat_id}_{note[1]})"
+            f"- [{note[0]}](https://t.me/{Config.BOT_USERNAME}?start=note_{chat_id}_{note[1]}_0)"
             for note in all_notes
         ]
-        rply += (
-            "\n".join(note_list)
-            + "You can retrieve these notes by tapping on the notename."
-        )
-
+        rply += "\n".join(note_list)
+        rply += "\n\nYou can retrieve these notes by tapping on the note-name."
         await m.reply_text(rply, disable_web_page_preview=True, quote=True)
         return
 
-    if len(help_lst) != 3:
+    if len(help_lst) < 3:
         return
 
     note_hash = help_option.split("_")[2]
+    raw = bool(int(help_option.split("_")[3]))
     getnotes = notes_db.get_note_by_hash(note_hash)
     if not getnotes:
-        await m.reply_text("Note does not exist", quote=True)
+        await m.reply_text("Note does not exist!", quote=True)
         return
 
     msgtype = getnotes["msgtype"]
@@ -106,90 +108,96 @@ async def get_private_note(c: Alita, m: Message, help_option: str):
     try:
         # support for random notes texts
         splitter = "%%%"
-        note_reply = getnotes["note_value"].split(splitter)
-        note_reply = choice(note_reply)
+        note_replu = getnotes["note_value"].split(splitter)
+        note_replu = choice(note_replu)
     except KeyError:
-        note_reply = ""
+        note_replu = ""
 
-    parse_words = [
-        "first",
-        "last",
-        "fullname",
-        "username",
-        "id",
-        "chatname",
-        "mention",
-    ]
-    text = await escape_mentions_using_curly_brackets(m, note_reply, parse_words)
-
-    if msgtype == Types.TEXT:
-
-        teks, button = await parse_button(text)
-        button = await build_keyboard(button)
-        button = ikb(button) if button else None
-        if button:
-            try:
-                await m.reply_text(
-                    teks,
-                    reply_markup=button,
-                    disable_web_page_preview=True,
-                    quote=True,
-                )
-                return
-            except RPCError as ef:
-                await m.reply_text(
-                    "An error has occurred! Cannot parse note.",
-                    quote=True,
-                )
-                LOGGER.error(ef)
-                LOGGER.error(format_exc())
-                return
-        else:
-            await m.reply_text(teks, quote=True, disable_web_page_preview=True)
-            return
-    elif msgtype in (
-        Types.STICKER,
-        Types.VIDEO_NOTE,
-        Types.CONTACT,
-        Types.ANIMATED_STICKER,
-    ):
-        await (await send_cmd(c, msgtype))(m.chat.id, getnotes["fileid"])
-    else:
-        if getnotes["note_value"]:
-            teks, button = await parse_button(getnotes["note_value"])
-            button = await build_keyboard(button)
-            button = ikb(button) if button else None
-        else:
-            teks = ""
-            button = None
-        if button:
-            try:
-                await (await send_cmd(c, msgtype))(
-                    m.chat.id,
-                    getnotes["fileid"],
-                    caption=teks,
-                    reply_markup=button,
-                )
-                return
-            except RPCError as ef:
-                await m.reply_text(
-                    teks,
-                    quote=True,
-                    reply_markup=button,
-                    disable_web_page_preview=True,
-                )
-                LOGGER.error(ef)
-                LOGGER.error(format_exc())
-                return
+    if raw:
+        if msgtype == Types.TEXT:
+            await m.reply_text(note_replu, parse_mode=ParseMode.DISABLED)
+        elif msgtype in (
+            Types.STICKER,
+            Types.VIDEO_NOTE,
+            Types.CONTACT,
+            Types.ANIMATED_STICKER,
+        ):
+            await (await send_cmd(c, msgtype))(
+                m.chat.id,
+                getnotes["fileid"],
+            )
         else:
             await (await send_cmd(c, msgtype))(
                 m.chat.id,
                 getnotes["fileid"],
-                caption=teks,
+                caption=note_replu,
+                parse_mode=ParseMode.DISABLED,
             )
-    LOGGER.info(
-        f"{m.from_user.id} fetched privatenote {note_hash} (type - {getnotes}) in {m.chat.id}",
+            LOGGER.info(
+                f"{m.from_user.id} fetched raw note {note_hash} (type - {getnotes}) in {int(help_lst[1])}",
+            )
+        return
+
+    parse_words = (
+        "first",
+        "last",
+        "fullname",
+        "username",
+        "mention",
+        "id",
+        "time",
+        "date",
+        "chatname",
+        "rules",
     )
+    texti = await escape_mentions_using_curly_brackets(
+        m, note_replu, parse_words
+    )
+    pre = "{preview}" not in texti
+    pro = "{protect}" in texti
+
+    textt, butto = await parse_button(
+        texti.replace("{preview}", "")
+            .replace("{private}", "")
+            .replace("{protect}", "")
+    )
+    buttn = await build_keyboard(butto)
+    button = InlineKeyboardMarkup(buttn) if buttn else None
+
+    try:
+        if msgtype == Types.TEXT:
+            await (await send_cmd(c, msgtype))(
+                m.chat.id,
+                textt,
+                reply_markup=button,
+                disable_web_page_preview=pre,
+                protect_content=pro,
+            )
+        elif msgtype in (
+            Types.STICKER,
+            Types.VIDEO_NOTE,
+            Types.CONTACT,
+            Types.ANIMATED_STICKER,
+        ):
+            await (await send_cmd(c, msgtype))(
+                m.chat.id,
+                getnotes["fileid"],
+                reply_markup=button,
+                protect_content=pro,
+            )
+        else:
+            await (await send_cmd(c, msgtype))(
+                m.chat.id,
+                getnotes["fileid"],
+                caption=textt,
+                reply_markup=button,
+                protect_content=pro,
+            )
+        LOGGER.info(
+            f"{m.from_user.id} fetched privatenote {note_hash} (type - {getnotes}) in {m.chat.id}",
+        )
+    except RPCError as e:
+        await m.reply_text(f"Error: {e.MESSAGE}")
     return
 
 
@@ -232,7 +240,14 @@ async def get_help_msg(m: Message or CallbackQuery, help_option: str):
             HELP_COMMANDS[i]["buttons"]
             for i in HELP_COMMANDS
             if help_option in HELP_COMMANDS[i]["alt_cmds"]
-        ) + [[("« " + (tlang(m, "general.back_btn")), "commands")]]
+        ) + [
+                      [
+                          InlineKeyboardButton(
+                              "« " + (tlang(m, "general.back_btn")),
+                              callback_data="commands",
+                          ),
+                      ],
+                  ]
         help_msg = (
             f"**{(tlang(m, (help_option_name['help_msg']).replace('.help', '.main')))}:**\n\n"
             + tlang(m, help_option_value)
@@ -244,7 +259,12 @@ async def get_help_msg(m: Message or CallbackQuery, help_option: str):
         help_msg = tlang(m, "general.commands_available")
         help_kb = [
             *(await gen_cmds_kb(m)),
-            [(f"« {(tlang(m, 'general.back_btn'))}", "start_back")],
+            [
+                InlineKeyboardButton(
+                    f"« {(tlang(m, 'general.back_btn'))}",
+                    callback_data="start_back",
+                ),
+            ],
         ]
 
     return help_msg, help_kb
