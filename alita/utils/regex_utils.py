b115730cc57e1c70585f28237fd90cca79a0dd7c
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-10-01 13:43:54 +0530

    Some commits.

--- alita/utils/regex_utils.py
+++ alita/utils/regex_utils.py
@@ -1,11 +1,12 @@
 from traceback import format_exc
 
 from regex import search
+from regex._regex import Match
 
 from alita import LOGGER
 
 
-async def regex_searcher(regex_string: str, string: str) -> str:
+async def regex_searcher(regex_string: str, string: str) -> bool | Match[str]:
     """Search for Regex in string."""
     try:
         re_search = search(regex_string, string, timeout=6)
