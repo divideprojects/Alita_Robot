7be01ee20431d2b46125e68f09391708835d1fbc
Divanshu Chauhan <divkix@divkix.me>
2025-08-14 18:53:52 -0700

    refactor: remove unused error constructors
    - Remove unused New() and Newf() functions from errors package
    - Keep actively used Wrap(), Wrapf(), and WrappedError type
    - Eliminates 2 unreachable functions (~34 lines)

--- alita/utils/errors/errors.go
+++ alita/utils/errors/errors.go
@@ -59,37 +59,3 @@ func Wrap(err error, message string) error {
 func Wrapf(err error, format string, args ...any) error {
 	return Wrap(err, fmt.Sprintf(format, args...))
 }
-
-func New(message string) error {
-	pc, file, line, ok := runtime.Caller(1)
-	if !ok {
-		return fmt.Errorf("%s", message)
-	}
-
-	fn := runtime.FuncForPC(pc)
-	funcName := "unknown"
-	if fn != nil {
-		funcName = fn.Name()
-		parts := strings.Split(funcName, "/")
-		if len(parts) > 0 {
-			funcName = parts[len(parts)-1]
-		}
-	}
-
-	parts := strings.Split(file, "/")
-	if len(parts) > 2 {
-		file = strings.Join(parts[len(parts)-2:], "/")
-	}
-
-	return &WrappedError{
-		Err:      fmt.Errorf("%s", message),
-		Message:  message,
-		File:     file,
-		Line:     line,
-		Function: funcName,
-	}
-}
-
-func Newf(format string, args ...any) error {
-	return New(fmt.Sprintf(format, args...))
-}
